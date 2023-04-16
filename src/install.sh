3b1f3a3a91758afbbfbb6e32c1bf7b8324666d79
LTXOM <ltxom@uw.edu>
2023-04-15 17:39:06 -0700

    An attempt to state-of-arts ATM model

---
+++ src/install.sh
@@ -0,0 +1,14 @@
+if [ ! -d "../venv" ]
+then
+  echo "Creating virtual environment..."
+  python3 -m venv "../venv"
+  source ../venv/bin/activate
+  echo "Installing packages..."
+  pip install h5py validators tqdm numpy argparse requests colorama
+fi
+source ../venv/bin/activate
+
+echo "Cloning the repository for SDK..."
+git clone https://github.com/A2Zadeh/CMU-MultimodalSDK.git
+export PYTHONPATH="$PWD/CMU-MultimodalSDK:$PYTHONPATH"
+pip install notebook
