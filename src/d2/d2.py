ae65b5ea86a15b8a9539501f7d0bb7975459ce22
LTXOM <ltxom@uw.edu>
2023-04-23 00:58:26 -0700

    Add visualization code for D2

--- src/d2/d2.py
+++ src/d2/d2.py
@@ -2,6 +2,10 @@
 # from mmsdk import mmdatasdk as md
 #
 # from sklearn.metrics import accuracy_score
+import pandas as pd
+import sklearn
+from matplotlib import pyplot as plt
+from sklearn.metrics import f1_score
 from sklearn.svm import SVR
 
 import numpy as np
@@ -10,6 +14,36 @@ from keras.layers import Dense
 from keras import Sequential
 
 from keras import backend as K
+
+# Seed value
+# Apparently you may use different seed values at each stage
+seed_value = 2023
+
+# 1. Set the `PYTHONHASHSEED` environment variable at a fixed value
+import os
+
+os.environ['PYTHONHASHSEED'] = str(seed_value)
+
+# 2. Set the `python` built-in pseudo-random generator at a fixed value
+import random
+
+random.seed(seed_value)
+
+# 3. Set the `numpy` pseudo-random generator at a fixed value
+np.random.seed(seed_value)
+
+# 4. Set the `tensorflow` pseudo-random generator at a fixed value
+import tensorflow as tf
+
+tf.random.set_seed(seed_value)
+# for later versions:
+# tf.compat.v1.set_random_seed(seed_value)
+
+# 5. Configure a new global `tensorflow` session
+# for later versions:
+session_conf = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+sess = tf.compat.v1.Session(graph=tf.compat.v1.get_default_graph(), config=session_conf)
+tf.compat.v1.keras.backend.set_session(sess)
 
 # # input data
 # text_field = './data/CMU_MOSI_TimestampedWordVectors'
@@ -156,7 +190,7 @@ model.compile()
 output = model.predict(x_train_ave[0].reshape(1, -1))
 
 model.compile('adam', 'mean_squared_error', metrics=['mse'])
-history_model = model.fit(x_train_ave, y_train_refined, batch_size=256, epochs=20, verbose=1)
+history_model = model.fit(x_train_ave, y_train_refined, batch_size=256, epochs=20, verbose=1, validation_data=(x_val_ave, y_val_refined))
 
 
 def coeff_determination(y_true, y_pred):
@@ -190,10 +224,84 @@ def get_accuracy_7(y_real, y_predict):
     return true_pos / len(y_real)
 
 
+def get_mae(y_real, y_predict):
+    mae = tf.keras.losses.MeanAbsoluteError()
+    return mae(y_real * 6 - 3, y_predict * 6 - 3).numpy()
+
+def get_classification_lists(y_real, y_predict):
+    true_list = []
+    predict_list = []
+    for i in range(len(y_real)):
+        if y_real[i] < 0.5:
+            true_list.append(0)
+        if y_predict[i] < 0.5:
+            predict_list.append(0)
+        if y_real[i] >= 0.5:
+            true_list.append(1)
+        if y_predict[i] >= 0.5:
+            predict_list.append(1)
+    return true_list, predict_list
+
+def get_f1(y_real, y_predict):
+    true_list, predict_list = get_classification_lists(y_real, y_predict)
+    return f1_score(true_list, predict_list, average='weighted')
+
+print("neural network F1: " + str(get_f1(y_dev_refined, y_dev_pred)))
+print("neural network MAE: " + str(get_mae(y_dev_refined, y_dev_pred)))
 print("neural network accuracy (binary): " + str(get_accuracy_2(y_dev_refined, y_dev_pred)))
-print("neural network accuracy (7 segments classification): " + str(get_accuracy_7(y_dev_refined, y_dev_pred)))
+print("neural network accuracy (7 class classification): " + str(get_accuracy_7(y_dev_refined, y_dev_pred)))
 print("neural network R^2: " + str(coeff_determination(y_dev_refined, y_dev_pred)))
+
 print()
-print("SVR network accuracy (binary): " + str(get_accuracy_2(y_val_refined, m.predict(x_val_ave))))
+print("SVR network F1: " + str(get_f1(y_dev_refined, m.predict(x_dev_ave))))
+print("SVR network MAE: " + str(get_mae(y_dev_refined, m.predict(x_dev_ave))))
+print("SVR network accuracy (binary): " + str(get_accuracy_2(y_dev_refined, m.predict(x_dev_ave))))
 print("SVR network accuracy (7 segments classification): " + str(get_accuracy_7(y_dev_refined, m.predict(x_dev_ave))))
 print('SVR network R^2:', test_score)
+
+
+print('Please check visualization outputs in the \'output\' directory.')
+
+nn_true_list, nn_predict_list = get_classification_lists(y_dev_refined, y_dev_pred)
+confusion_matrix_org = sklearn.metrics.confusion_matrix(nn_true_list, nn_predict_list)
+bal_plt = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix_org, display_labels=[True, False])
+bal_plt.plot(cmap=plt.cm.Blues)
+bal_plt.figure_.savefig('../../outputs/neural_network_conf_mat.png')
+plt.clf()
+
+svr_true_list, svr_predict_list = get_classification_lists(y_dev_refined, m.predict(x_dev_ave))
+confusion_matrix_org = sklearn.metrics.confusion_matrix(svr_true_list, svr_predict_list)
+bal_plt = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix_org, display_labels=[True, False])
+bal_plt.plot(cmap=plt.cm.Blues)
+bal_plt.figure_.savefig('../../outputs/svr_conf_mat.png')
+plt.clf()
+
+fpr_nn, tpr_nn, _ = sklearn.metrics.roc_curve(nn_true_list, nn_predict_list)
+
+fpr_svr, tpr_svr, _ = sklearn.metrics.roc_curve(svr_true_list, svr_predict_list)
+
+nn_auc = sklearn.metrics.roc_auc_score(nn_true_list, nn_predict_list)
+svr_auc = sklearn.metrics.roc_auc_score(svr_true_list, svr_predict_list)
+
+plt.plot(fpr_nn, tpr_nn, label="Neural_Network_AUC=" + str(nn_auc))
+plt.plot(fpr_svr, tpr_svr, label="SVR_AUC=" + str(svr_auc))
+
+plt.title('ROC (receiver operating characteristic)')
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+
+plt.legend(loc=4)
+plt.savefig('../../outputs/ROC.png')
+plt.clf()
+
+hist = pd.DataFrame(history_model.history)
+fig, ax = plt.subplots()
+ax.plot(hist.index, hist['loss'], label='Training Loss')
+ax.plot(hist.index, hist['val_loss'], label='Validation Loss')
+ax.set_ylabel('loss')
+ax.set_xlabel('epochs')
+plt.title('Loss vs. Epochs')
+ax.legend(loc='upper left')
+
+plt.savefig('../../outputs/neural_network_loss.png')
+
